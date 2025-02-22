05bdb1adef242f9b6181b10df3072e36ee384a19
Imran Chouybat <imran.chouybat1@gmail.com>
2025-02-22 10:26:18 +0100

    Configure setup.py to install dependecies. Added .egg-info file to .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Reports Classification",
+    version="0.1.0",
+    description="A BERT-based accident/maintenance reporots classification",
+    author="Imran Chouybat",
+    author_email="imran.chouybat1@gmail.com",
+    packages=find_packages()
+)
